994b912ee9991401b42a30c59e5035737bfeb3e3
gabriel <89615327+gabrielentediado@users.noreply.github.com>
2024-08-20 15:17:15 -0300

    Update bash.sh

--- 6.observação/bash.sh
+++ 6.observação/bash.sh
@@ -1,7 +1,9 @@
 #vou criar um arquivo .txt na sua área de trabalho:
 #isso daqui é só pra procurar o arquivo e evitar erros:
 
-arquivo = $(find/-name "2.abrir.c" 2>/dev/null)
+#!/bin/bash
+
+arquivo=$(find ~ -name "2.abrir.c" 2>/dev/null)
 
 if [ -z "$arquivo" ]; then
     echo "Arquivo 2.abrir.c não encontrado."
